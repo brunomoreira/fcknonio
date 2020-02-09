4da11a8379ca0467bd4e9914c634253f3fbc4d7c
XpekT <xpektpt@gmail.com>
2020-02-09 00:13:09 +0000

    updated links and elements to search for

--- content.js
+++ content.js
@@ -1,14 +1,45 @@
+function hasClass(element, className) {
+    return (' ' + element.className + ' ').indexOf(' ' + className+ ' ') > -1;
+}
+
 window.onload = function() {
 
-    let regex = /layer_gatting?=*/i
+    let regexes = [
+        /layer_gatting?=*/i,
+        /imp-content-gate-root?=*/i,
+        /wrapperContentGatingNonio?=*/i
+    ]
+
+    let classes = [
+        "tp-modal", 
+        "tp-backdrop tp-active",
+        "nonioBox",
+        "pop-out-lightbox",
+        "warning-nonio-overlay"
+    ]
+
     let divs = document.getElementsByTagName('div')
 
     for(let div of divs) {
 
-        if(regex.test(div.id)) {
-            div.remove()
-        }
-   
+        regexes.forEach(function(regex) {
+
+            if(
+                regex.test(div.id)
+            ) {
+                div.remove()
+            }
+        
+        })
+        
+        classes.forEach(function(item) {
+
+            if(hasClass(div, item)) {
+                div.remove()
+            }
+
+        })
     }
+
 
 }
